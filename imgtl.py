1a9d9472563ec626743f4116b6965b6f0394bc31
Edina Harsay <harsay@ku.edu>
2021-10-31 06:17:06 -0500

    Updated imgtl.py to match .ipynb file

--- imgtl.py
+++ imgtl.py
@@ -7,47 +7,51 @@ from PIL import ImageFont, ImageDraw, Image
 # (Check path for tesseract and modify the line below if needed)
 pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
 
+# This function handles the core OCR processing of the input image.
+def ocr_core(filename, language):
+    choice = 0
+    if language == '1':
+        choice = 'chi_sim'
+    elif language == '2':
+        choice = 'deu'
+    elif language == '3':
+        choice = 'hun'
 
-def ocr_core(filename):
-    """
-    This function will handle the core OCR processing of images.
-    """
-    text = pytesseract.image_to_string(Image.open(filename), lang='chi_sim')
+    text = pytesseract.image_to_string(Image.open(filename), lang=choice)
     return text
 
-#writes text extracted from image into a txt file named "script.txt"
+# Writes text extracted from image into a txt file named "script.txt"
+# which is saved in the same path as the input image file
 def writefile(image_path, strstr):
 	wordlist = strstr.split()
 	with open(image_path + '/script.txt', 'a') as f:
 		f.writelines("\r".join(wordlist))
 
-# takes text extracted from txt file and translates them, outputs translated
-# version into a txt file called "translated.txt" and outputs an image
-# file called Capture.JPG
+# Takes lines of text extracted from txt file and translates them, outputs translated
+# version into a txt file called "translated.txt" and also outputs an image
+# file with the translated text called Capture.JPG.
 def translate_text(image_path):
+    # sets target language and input language for translator
     translator = Translator(to_lang="en", from_lang="zh")
+
+    # sets the dimensions and background color for the new image object
     width = 500
     height = 500
     im = Image.new(mode="RGB", size=(width, height), color=(255, 255, 255))
 
+    # opens the OCR output file and reads each line into contents
     with open(image_path + '/script.txt', encoding='utf-8') as f:
         contents = f.readlines()
 
+    # Generates a JPG image file from the translated txt file
     with open(image_path + '/translated.txt', 'a') as n:
-        for line in contents:
+        for line in contents:   # iterates thru each line of text in "contents"
             n.writelines(translator.translate(line) + "\n")
             draw = ImageDraw.Draw(im)
             font = ImageFont.truetype("arial.ttf", 15)
-            for i in range(9):
-                draw.text((10, i*14), translator.translate(line),
-                          fill=(0, 0, 0, 0), font=font)
+            draw.text((10, 14), translator.translate(line), fill=(0, 0, 0, 0), font=font)
         im.save("Capture.JPG")
         im.show()
-
-
-# obtain input path and number of files from the user
-path = input('file path: ')
-nums = input('# of files: ')
 
 # Change the translator provider
 # Default is mymemory but it limits the number of translations per day.
@@ -55,22 +59,23 @@ nums = input('# of files: ')
 # secret = '<your secret from Microsoft or DeepL>'
 # translator = Translator(provider='<the name of the provider, eg. microsoft or deepl>', to_lang=to_lang, secret_access_key=secret)
 
-# I signed up for DeepL account and got this access key but is seems not to be working because mymemory is still used
-# Code still works if not commented out but deepl is not used. Leaving code here for reference
-# (in case someone can fix it) but we should delete it if we can't fix it.
-# secret = '4ab5c119-0001-6b9d-8f3a-a7963bbabbe3:fx'
-# translator = Translator(provider='deepl', to_lang="en", secret_access_key=secret)
 
+# Obtain input path and file name from the user
+path = input('file path: ')
+userfile = input('file name: ')
+
+print("You have 3 choices for input language. Eneter 1 for Chinese, 2 for German, or 3 for Hungarian.")
+inlanguage = input('Your choice: ')
+print('\n')
 
 print("******Starting******")
 
-# This performs the OCR and writes a text file caled script.txt
-for n in range(1, int(nums)+1):
-    text = ocr_core(path + "/" + str(n) + ".png")
-    writefile(path, text)
-    print("OCR of image " + str(n) + " is done")
+# Perform OCR on the input image file and let user know when finished this step
+text = ocr_core(path + "/" + userfile, inlanguage)
+writefile(path, text)
+print("OCR of image " + userfile + " is done.")
 
-# This performs the translation step (it may take a while)
+# Perform the translation step
 print("Now performing the translation.")
 translate_text(path)
 
