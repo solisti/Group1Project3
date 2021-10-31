d00617860cf9e04a91e897f2fa8d31c906cce9bc
Edina Harsay <harsay@ku.edu>
2021-10-30 23:44:45 -0500

    Updated imgtl.py to match .ipynb code

--- imgtl.py
+++ imgtl.py
@@ -1,6 +1,12 @@
+from PIL import Image
 import pytesseract
-from PIL import Image, ImageFont, ImageDraw
 from translate import Translator
+from PIL import ImageFont, ImageDraw, Image
+
+# The next line may not be needed. It is needed if tesseract is not in your PATH
+# (Check path for tesseract and modify the line below if needed)
+pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+
 
 def ocr_core(filename):
     """
@@ -10,18 +16,23 @@ def ocr_core(filename):
     return text
 
 #writes text extracted from image into a txt file named "script.txt"
+
+
 def writefile(image_path, strstr):
 	wordlist = strstr.split()
 	with open(image_path + '/script.txt', 'a') as f:
 		f.writelines("\r".join(wordlist))
 
-#takes text extracted from txt file and translates them, outputs translated version into a txt file called "translated.txt"
+# takes text extracted from txt file and translates them, outputs translated
+# version into a txt file called "translated.txt" and outputs an image
+# file called Capture.JPH.
+
+
 def translate_text(image_path):
     translator = Translator(to_lang="en", from_lang="zh")
     width = 1000
     height = 1000
-    im  = Image.new( mode = "RGB", size = (width, height), color = (255, 255, 255) )
-    translator = Translator(to_lang="en", from_lang="zh")
+    im = Image.new(mode="RGB", size=(width, height), color=(255, 255, 255))
 
     with open(image_path + '/script.txt', encoding='utf-8') as f:
         contents = f.readlines()
@@ -32,20 +43,38 @@ def translate_text(image_path):
             draw = ImageDraw.Draw(im)
             font = ImageFont.truetype("arial.ttf", 15)
             for i in range(9):
-                draw.text((10, i*14), translator.translate(line), fill=(0,0,0,0), font=font)
-        im.save("Capture.JPG");
-        im.show();
+                draw.text((10, i*14), translator.translate(line),
+                          fill=(0, 0, 0, 0), font=font)
+        im.save("Capture.JPG")
+        im.show()
 
+
+# obtain input path and number of files from the user
 path = input('file path: ')
 nums = input('# of files: ')
 
-print("******starting******")
+# Change the translator provider
+# Default is mymemory but it limits the number of translations per day.
+# Template code to change provider:
+# secret = '<your secret from Microsoft or DeepL>'
+# translator = Translator(provider='<the name of the provider, eg. microsoft or deepl>', to_lang=to_lang, secret_access_key=secret)
 
+# I signed up for DeepL account and got this access key but is seems not to be working because mymemory is still used
+secret = '4ab5c119-0001-6b9d-8f3a-a7963bbabbe3:fx'
+translator = Translator(provider='deepl', to_lang="en",
+                        secret_access_key=secret)
+
+
+print("******Starting******")
+
+# This performs the OCR and writes a text file caled script.txt
 for n in range(1, int(nums)+1):
     text = ocr_core(path + "/" + str(n) + ".png")
     writefile(path, text)
-    print("image " + str(n) + " is done")
+    print("OCR of image " + str(n) + " is done")
 
-print("~~~~~finished~~~~~")
-
+# This performs the translation step (it may take a while)
+print("Now performing the translation.")
 translate_text(path)
+
+print("~~~~~~Finished~~~~~~")
